8212b193c2b304a47a51261ca77c250cd8c525fa
mrakhmanov <mrakhmanov@diasoft.ru>
2025-07-25 15:32:33 +0300

    HW2 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,46 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(str string) (string, error) {
+	var builder strings.Builder
+	var previousRune rune
+	markNextAsRune := false
+	for _, r := range str {
+		switch {
+		case previousRune == rune(0) && unicode.IsDigit(r):
+			return "", ErrInvalidString
+		case unicode.IsDigit(r) && !markNextAsRune:
+			count, err := strconv.Atoi(string(r))
+			if err != nil {
+				return "", err
+			}
+			builder.WriteString(strings.Repeat(string(previousRune), count))
+			previousRune = rune(0)
+		case r == '\\' && !markNextAsRune:
+			markNextAsRune = true
+		default:
+			if unicode.IsLetter(r) && markNextAsRune {
+				return "", ErrInvalidString
+			}
+			if previousRune != rune(0) {
+				builder.WriteRune(previousRune)
+			}
+			markNextAsRune = false
+			previousRune = r
+		}
+	}
+	if markNextAsRune {
+		return "", ErrInvalidString
+	}
+	if previousRune != rune(0) {
+		builder.WriteRune(previousRune)
+	}
+	return builder.String(), nil
 }
