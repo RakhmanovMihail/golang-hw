cb18a51b4f38ffd5e6a5dccd114ad0f52a5e4e7b
mrakhmanov <mrakhmanov@diasoft.ru>
2025-10-17 17:18:40 +0300

    HW7

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"io"
+	"os"
 )
 
 var (
@@ -10,6 +13,96 @@ var (
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	//32 KB
+	bufferSize := 32 * 1024
+
+	file, err := os.Open(fromPath)
+	if err != nil {
+		return err
+	}
+	defer func(file *os.File) {
+		_ = file.Close()
+	}(file)
+
+	fileState, err := file.Stat()
+	if err != nil {
+		return err
+	}
+	if !fileState.Mode().IsRegular() {
+		return ErrUnsupportedFile
+	}
+	if offset < 0 {
+		return errors.New("negative offset")
+	}
+	if limit < 0 {
+		return errors.New("negative limit")
+	}
+
+	fileSize := fileState.Size()
+	if offset > fileSize {
+		return ErrOffsetExceedsFileSize
+	}
+
+	data := make([]byte, bufferSize)
+	_, err = file.Seek(offset, io.SeekStart)
+	if err != nil {
+		return err
+	}
+	outFile, err := os.Create(toPath)
+	if err != nil {
+		return err
+	}
+	defer func(out *os.File) { _ = out.Close() }(outFile)
+
+	var remaining int64
+	if limit == 0 {
+		remaining = fileSize - offset
+	} else {
+		remaining = limit
+		if remaining > fileSize-offset {
+			remaining = fileSize - offset
+		}
+	}
+
+	totalToCopy := remaining
+	lastPct := -1
+	printProgress := func(done, total int64) {
+		if total <= 0 {
+			return
+		}
+		pct := int(done * 100 / total)
+		if pct != lastPct {
+			lastPct = pct
+			fmt.Printf("\rProgress: %d%%", pct)
+		}
+	}
+
+	for remaining > 0 {
+		toRead := data
+		if int64(len(toRead)) > remaining {
+			toRead = data[:remaining]
+		}
+
+		n, err := file.Read(toRead)
+		if n > 0 {
+			if _, werr := outFile.Write(toRead[:n]); werr != nil {
+				return werr
+			}
+			remaining -= int64(n)
+			if totalToCopy > 0 {
+				done := totalToCopy - remaining
+				printProgress(done, totalToCopy)
+			}
+		}
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return err
+		}
+	}
+	if totalToCopy > 0 {
+		fmt.Printf("\rProgress: %3d%%\n", 100)
+	}
 	return nil
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,86 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	tests := []struct {
+		name        string
+		offset      int64
+		limit       int64
+		expectError bool
+	}{
+		{"offset 0, limit 0", 0, 0, false},
+		{"offset 0, limit 10", 0, 10, false},
+		{"offset 0, limit 1000", 0, 1000, false},
+		{"offset 0, limit 10000", 0, 10000, false},
+		{"offset 100, limit 1000", 100, 1000, false},
+		{"offset 6000, limit 1000", 6000, 1000, false},
+		{"offset beyond file size", 100000, 1000, true},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			outputFile := "test_out_" + tc.name + ".txt"
+			defer func(name string) {
+				err := os.Remove(name)
+				if err != nil {
+
+				}
+			}(outputFile)
+
+			err := Copy("testdata/input.txt", outputFile, tc.offset, tc.limit)
+
+			if tc.expectError {
+				assert.Error(t, err)
+				return
+			}
+
+			require.NoError(t, err)
+
+			_, err = os.Stat(outputFile)
+			assert.NoError(t, err, "output file should exist")
+
+		})
+	}
+}
+
+func TestCopy_Validation(t *testing.T) {
+	tests := []struct {
+		name        string
+		src         string
+		offset      int64
+		limit       int64
+		expectError bool
+	}{
+		{"non-existent source", "nonexistent.txt", 0, 10, true},
+		{"directory as source", "testdata", 0, 10, true},
+		{"negative offset", "testdata/input.txt", -1, 10, true},
+		{"negative limit", "testdata/input.txt", 0, -1, true},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			outputFile := "test_validation_" + tc.name + ".txt"
+			defer func(name string) {
+				err := os.Remove(name)
+				if err != nil {
+					t.Logf("failed to remove file: %v", err)
+				}
+			}(outputFile)
+
+			err := Copy(tc.src, outputFile, tc.offset, tc.limit)
+
+			if tc.expectError {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+			}
+		})
+	}
 }
