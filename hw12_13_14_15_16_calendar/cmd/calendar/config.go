4eb50099e3ecdd7c1bf0f1e60aab6fa2278a8820
mrakhmanov <mrakhmanov@diasoft.ru>
2026-03-27 12:25:35 +0300

    Точка входа, конфигурирование и зависимости

--- hw12_13_14_15_16_calendar/cmd/calendar/config.go
+++ hw12_13_14_15_16_calendar/cmd/calendar/config.go
@@ -1,20 +1,70 @@
 package main
 
-// При желании конфигурацию можно вынести в internal/config.
-// Организация конфига в main принуждает нас сужать API компонентов, использовать
-// при их конструировании только необходимые параметры, а также уменьшает вероятность циклической зависимости.
+import (
+	"fmt"
+	"strings"
+
+	"github.com/BurntSushi/toml"
+	logger "github.com/RakhmanovMihail/golang-hw/hw12_13_14_15_16_calendar/internal/logger"
+	"log"
+)
+
+// Локальный тип для TOML десериализации
+type tomlLevel logger.Level
+
+func (l *tomlLevel) UnmarshalTOML(data []byte) error {
+	level := strings.Trim(string(data), `"`)
+	switch level {
+	case string(logger.LevelDebug), string(logger.LevelInfo),
+		string(logger.LevelWarn), string(logger.LevelError):
+		*l = tomlLevel(level)
+		return nil
+	default:
+		return fmt.Errorf("invalid log level: %s", level)
+	}
+}
+
 type Config struct {
-	Logger LoggerConf
-	// TODO
+	Logger  LoggerConf    `toml:"logger"`
+	Api     ApiConfig     `toml:"api"`
+	Storage StorageConfig `toml:"storage"`
+}
+
+// Конвертеры уровней
+func (c *Config) LoggerLevel() logger.Level {
+	return logger.Level(c.Logger.Level)
+}
+
+func (c *Config) ApiLevel() logger.Level {
+	return logger.Level(c.Api.Level)
+}
+
+type ApiConfig struct {
+	Level tomlLevel `toml:"level"`
+	Host  string    `toml:"host"`
+	Port  string    `toml:"port"`
 }
 
 type LoggerConf struct {
-	Level string
-	// TODO
+	Level tomlLevel `toml:"level"`
 }
 
-func NewConfig() Config {
-	return Config{}
+type StorageMode string
+
+const (
+	StorageModeMemory StorageMode = "memory"
+	StorageModeSQL    StorageMode = "sql"
+)
+
+type StorageConfig struct {
+	Mode StorageMode `toml:"mode"`
+	DSN  string      `toml:"dsn,omitempty"` // для SQL
 }
 
-// TODO
+func NewConfig(path string) *Config {
+	var config Config
+	if _, err := toml.DecodeFile(path, &config); err != nil {
+		log.Fatal(err)
+	}
+	return &config
+}
--- hw12_13_14_15_16_calendar/cmd/calendar/main.go
+++ hw12_13_14_15_16_calendar/cmd/calendar/main.go
@@ -2,60 +2,110 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	"flag"
+	"fmt"
+	"log"
 	"os"
 	"os/signal"
 	"syscall"
 	"time"
 
-	"github.com/fixme_my_friend/hw12_13_14_15_16_calendar/internal/app"
-	"github.com/fixme_my_friend/hw12_13_14_15_16_calendar/internal/logger"
-	internalhttp "github.com/fixme_my_friend/hw12_13_14_15_16_calendar/internal/server/http"
-	memorystorage "github.com/fixme_my_friend/hw12_13_14_15_16_calendar/internal/storage/memory"
+	"github.com/BurntSushi/toml"
+	"github.com/pressly/goose/v3"
+
+	app "github.com/RakhmanovMihail/golang-hw/hw12_13_14_15_16_calendar/internal/app"
+	loggerpkg "github.com/RakhmanovMihail/golang-hw/hw12_13_14_15_16_calendar/internal/logger"
+	internalhttp "github.com/RakhmanovMihail/golang-hw/hw12_13_14_15_16_calendar/internal/server/http"
+	storage "github.com/RakhmanovMihail/golang-hw/hw12_13_14_15_16_calendar/internal/storage"
+	memorystorage "github.com/RakhmanovMihail/golang-hw/hw12_13_14_15_16_calendar/internal/storage/memory"
+	migrations "github.com/RakhmanovMihail/golang-hw/hw12_13_14_15_16_calendar/migrations"
+	psqlstorage "github.com/RakhmanovMihail/golang-hw/hw12_13_14_15_16_calendar/internal/storage/sql"
+	_ "github.com/lib/pq"
 )
 
-var configFile string
+var configPath string
 
 func init() {
-	flag.StringVar(&configFile, "config", "/etc/calendar/config.toml", "Path to configuration file")
+	flag.StringVar(&configPath, "config", "configs/config.toml", "Path to configuration file")
 }
 
 func main() {
 	flag.Parse()
-
-	if flag.Arg(0) == "version" {
-		printVersion()
+	if len(os.Args) > 1 && os.Args[1] == "version" {
+		fmt.Println("calendar version develop")
 		return
 	}
 
-	config := NewConfig()
-	logg := logger.New(config.Logger.Level)
+	cfg := &Config{}
+	_, err := toml.DecodeFile(configPath, cfg)
+	if err != nil {
+		log.Fatalf("config %s: %v", configPath, err)
+	}
 
-	storage := memorystorage.New()
-	calendar := app.New(logg, storage)
+	logg := loggerpkg.New(cfg.LoggerLevel())
 
-	server := internalhttp.NewServer(logg, calendar)
+	var store storage.Storage
+	switch cfg.Storage.Mode {
+	case "memory":
+		store = memorystorage.New()
+	case "postgres":
+		// Миграции ПЕРЕД созданием storage!
+		if err := runMigrations(logg, cfg.Storage.DSN); err != nil {
+			logg.Error(fmt.Sprintf("migrations: %v", err))
+			os.Exit(1)
+		}
 
-	ctx, cancel := signal.NotifyContext(context.Background(),
-		syscall.SIGINT, syscall.SIGTERM, syscall.SIGHUP)
+		storeSQL, err := psqlstorage.New(cfg.Storage.DSN)
+		if err != nil {
+			logg.Error(fmt.Sprintf("postgres: %v", err))
+			os.Exit(1)
+		}
+		store = storeSQL
+	default:
+		logg.Error(fmt.Sprintf("unknown storage: %s", cfg.Storage.Mode))
+		os.Exit(1)
+	}
+
+	calendar := app.New(*logg, store)
+	server := internalhttp.NewServer(logg, calendar, fmt.Sprintf("%s:%s", cfg.Api.Host, cfg.Api.Port))
+
+	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM, syscall.SIGHUP)
 	defer cancel()
 
 	go func() {
 		<-ctx.Done()
-
-		ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
-		defer cancel()
-
-		if err := server.Stop(ctx); err != nil {
-			logg.Error("failed to stop http server: " + err.Error())
+		shutdownCtx, shutdownCancel := context.WithTimeout(context.Background(), 3*time.Second)
+		defer shutdownCancel()
+		if err := server.Stop(shutdownCtx); err != nil {
+			logg.Error(fmt.Sprintf("server stop: %v", err))
 		}
 	}()
 
 	logg.Info("calendar is running...")
-
 	if err := server.Start(ctx); err != nil {
-		logg.Error("failed to start http server: " + err.Error())
-		cancel()
-		os.Exit(1) //nolint:gocritic
+		logg.Error(fmt.Sprintf("server start: %v", err))
+		os.Exit(1)
 	}
+}
+
+func runMigrations(logger *loggerpkg.Logger, dsn string) error {
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		return fmt.Errorf("open db: %w", err)
+	}
+	defer db.Close()
+
+	goose.SetBaseFS(migrations.FS)
+
+	if err := goose.SetDialect("postgres"); err != nil {
+		return fmt.Errorf("set dialect: %w", err)
+	}
+
+	if err := goose.Up(db, "."); err != nil {
+		return fmt.Errorf("goose up: %w", err)
+	}
+
+	logger.Info("migrations applied successfully")
+	return nil
 }
